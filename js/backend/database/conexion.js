54167e4a326d3a430982abb830e7f314de2edb0f
andrwe455 <andrwe455@gmail.com>
2023-10-17 08:54:06 -0500

    backend D1Qawsedcxf vd

--- js/backend/database/conexion.js
+++ js/backend/database/conexion.js
@@ -1,17 +1,27 @@
 const mongoose = require('mongoose');
-
-
-async function connect () {
-
+async function connect (req, res) {
+    const { user,password }=req.body;
     try {
-        mongoose.connect(`mongodb+srv://andresgutierrez83111:fO1RwkAfBKaGKFA8@cluster0.labzm9a.mongodb.net/taller2?retryWrites=true&w=majority`);
+        mongoose.connect(`mongodb+srv://${user}:${password}@cluster0.labzm9a.mongodb.net/taller2?retryWrites=true&w=majority`);
+        res.status(200).json({msg:"Conexion exitosa"});
         console.log('DB ONLINE');
     } catch (error) {
         console.log(error);
         throw new Error('Error a la hora de inicializar base de datos');
     }
 }
+async function disconnect (req, res) {
+    try {
+        mongoose.disconnect();
+        res.status(200).json({msg:"Desconexion exitosa"});
+        console.log('DB OFFLINE');
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error a la hora de desconectar base de datos');
+    }
+}
 
 module.exports = {
-    connect
+    connect,
+    disconnect
 }
--- js/backend/rutas/routes.js
+++ js/backend/rutas/routes.js
@@ -2,12 +2,15 @@ const { express } = require('express');
 const {Router} = require('express');
 const router = Router();
 const controlador = require('../controller/controlador');
+const {check, validationResult} = require('express-validator');
+const conexion = require('../database/conexion.js');
 
 
 
-
+router.post('/conexion', conexion.connect);
+router.post('/desconexion', conexion.disconnect);
 router.post('/nuevo', controlador.signup);
-router.post('/login', controlador.login);
+// router.post('/login', controlador.login);
 router.post('/crearmascota', controlador.Crearmascota);
 router.post('/crearhistoria',controlador.Crearhistoria);
 
