00d1e7639ac8a1d8bdb698bf0bb4eb15461d8606
Mariana <Mariana36652@gmail.com>
2023-12-05 17:38:04 -0500

    Perfil de Usuario

---
+++ Front/Pagina/js/userprofilescript.js
@@ -0,0 +1,10 @@
+function previewImage(event) {
+    var input = event.target;
+    var preview = document.getElementById('preview');
+  
+    var reader = new FileReader();
+    reader.onload = function() {
+      preview.src = reader.result;
+    }
+    reader.readAsDataURL(input.files[0]);
+  }
